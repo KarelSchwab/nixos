2d2fa2796a087e2cd7aa11912b85317a83e5b4a8
Karel Schwab <schwabk9@protonmail.com>
2024-06-15 21:16:06 +0200

    Add volume progress bar

--- scripts/.local/bin/volume.sh
+++ scripts/.local/bin/volume.sh
@@ -2,7 +2,7 @@
 
 function send_notification {
     volume=$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')
-    dunstify -t 1000 -r 69420 -u normal "Volume: $volume%"
+    dunstify -t 1000 -r 69420 -u normal -h int:value:"$volume" "Volume: $volume%"
 }
 
 case $1 in
